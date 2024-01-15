144dcd0ab9809cfea7e26629132063f9fd156ea3
dolbix-kfukuda <156416027+Kunika-Fukuda@users.noreply.github.com>
2024-01-15 16:04:42 +0900

    Create 32.js

---
+++ 32.js
@@ -0,0 +1,7 @@
+
+$('div#main').mouseover(function(){
+
+    $('div.lgc-chart-parallelView-item').removeClass('lgc-dateRangeChangeable');
+    $('div.lgc-chart-object').removeClass('lgc-dateRangeChangeable');
+
+});
