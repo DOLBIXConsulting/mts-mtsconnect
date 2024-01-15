753a0224902b936a54eafcdd266daddf0d921f52
dolbix-kfukuda <156416027+Kunika-Fukuda@users.noreply.github.com>
2024-01-15 16:03:07 +0900

    Create 08.js

---
+++ 08.js
@@ -0,0 +1,9 @@
+$(function(){
+    $(".select2-hidden-accessible").css("background-color","#00ff7f");
+    $("#issue_status_id").css("background-color","#00ff7f");
+    // 3/24 追加
+    $("#issue_group_id").css("background-color","#00ff7f");
+    // 4/26 追加
+    $('#issue_custom_field_values_22').css("background-color","#00ff7f");
+
+});
